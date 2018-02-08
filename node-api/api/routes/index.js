0fb8bbff69ea824d7c59b6dd18b00ccedbef5a30
Quentin Kabasele <qkabasele@gmail.com>
2018-02-08 11:52:02 +0100

    Fix

--- node-api/api/routes/index.js
+++ node-api/api/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const message = "Route INDEX";
-    res.send(message);
+    const message = "Appel à la route INDEX";
     console.log(message);
+    res.send(message);
+    
 });
 
 module.exports = router;
